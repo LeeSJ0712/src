1849a26f37c1a2e3e0d9e57b12bf8f71c46399a3
Seunghun Lee <qjatjtmdwns@naver.com>
2022-11-01 22:35:33 +0900

    2번

--- src/woowa2/Value.java
+++ src/woowa2/Value.java
@@ -1,10 +1,33 @@
 package woowa2;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class Value {
-
+	static int global_flag=0;
+	public static String cutting(String str) {
+		int s_len = str.length();
+		char [] chars= str.toCharArray();
+		for(int x=0;x<s_len-1;x++) {
+			if(chars[x] == chars[x+1]&& chars[x] != '"') {
+				chars[x] =' ';
+				chars[x+1] = ' ';
+				global_flag=1;
+			}
+		}
+		String a = new String(chars);
+		a= a.replaceAll(" ", "");
+		return a;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Scanner scan = new Scanner(System.in);
+        String str;
+        str = scan.nextLine();
+        while(true) {
+        	global_flag=0;
+        	str = cutting(str);
+        	if(global_flag==0)break;
+        }
+        System.out.println(str);
 
 	}
-
 }
